819eed6a941bcb00bee1405aba6b5dd4b12e30b5
Лев <slarktiv34@gmail.com>
2023-07-19 18:03:29 +0300

    wish-list

--- views/site/map.php
+++ views/site/map.php
@@ -2,20 +2,23 @@
     <h1 class="text-center mt-4 mb-4">Организационные вопросы.</h1>
     <div>
         <p>Мероприятие пройдёт: 2 сентября в 14:00</p>
-        <div class="row text-white pb-5 timer-block">
+        <div class="row text-white timer-block">
             <hr class="style-volna2">
             <script src="/js/timer2.js"></script>
             <hr class="style-volna2 mt-4">
         </div>
         <p>Место: Ресторан «Времена года» в Центральном Парке Культуры и Отдыха им. Горького. Главный вход в ресторан на первом этаже.</p>
         <p>Адрес: ул. Крымский Вал, 9, стр.20, Москва</p>
+        <div class="mt-4 mb-4">
+            <script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A977ad6799fb325929a0137e4a44c5ed6f4ee48900b164e1b1a1d3a18aec05a41&amp;width=100%25&amp;height=240&amp;lang=ru_RU&amp;scroll=true"></script>
+        </div>
         <p>Дресс код:</p>
         <ul>
             <li class="marker-int-white">На торжество: каблуки и платье у девушек, элегантный вид мужчин, дети - в чистом. </li>
             <li class="marker-int-white">На танцы и прогулки по парку ночью – удобную обувь и комфортную одежду.</li>
         </ul>
         <p>В программе предусмотрены:</p>
-        <ul>
+        <ul class="pb-3">
             <li class="marker-int-white">Фуршет с 14:00 до 15:30.</li>
             <li class="marker-int-white">Будет фотозона с камином и отличный фотограф.  Задачка у фотографа сложная – сделать КАЖДОЙ паре и семье фото с праздника на память. Желательно, фото сделать во время приветственного фуршета, пока все свежие и культурные. Прошу всех приходить с 14:00.</li>
             <li class="marker-int-white">В 15:30 ОБЩАЯ ФОТО на память!</li>
@@ -27,8 +30,6 @@
             <li class="marker-int-white">С 21:00 дискотека с диджеем «для стойких» </li>
             <li class="marker-int-white">В 23:59 2 сентября «я календарь переверну!»</li>
         </ul>
-        <div>
-            <script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A977ad6799fb325929a0137e4a44c5ed6f4ee48900b164e1b1a1d3a18aec05a41&amp;width=100%25&amp;height=240&amp;lang=ru_RU&amp;scroll=true"></script>
-        </div>
+
     </div>
 </div>
